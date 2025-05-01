17b2ff4191e857b27adf62e939e7b896b9e1ff44
andrii563 <seniv.2005.a@gmail.com>
2025-05-01 03:05:57 +0200

    Update webhook_server.py

--- webhook_server.py
+++ webhook_server.py
@@ -6,7 +6,7 @@ import telegram
 app = Flask(__name__)
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-CHAT_ID = os.environ.get("CHAT_ID")  # наприклад: -1002635000565
+CHAT_ID = int(os.environ.get("CHAT_ID"))
 
 bot = telegram.Bot(token=BOT_TOKEN)
 
@@ -16,11 +16,13 @@ def webhook():
     print("Отримано webhook:", data)
 
     try:
-        # Перевірка статусу оплати
         if data.get("transactionStatus") == "Approved":
-            user_id = int(data["orderReference"].split(":")[1])  # orderReference = "subscription:USER_ID"
+            order_ref = data.get("orderReference", "")
+            if ":" not in order_ref:
+                return "Невірний формат orderReference", 400
+
+            user_id = int(order_ref.split(":")[1])
             bot.unban_chat_member(chat_id=CHAT_ID, user_id=user_id, only_if_banned=True)
-            bot.invite_link_create(chat_id=CHAT_ID)  # для публічних чатів можна генерувати інвайт
             return "OK", 200
         else:
             return "Оплата не підтверджена", 400
