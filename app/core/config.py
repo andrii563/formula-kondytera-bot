02e05d60f2d18a605567747d157d36c1be8c01f7
Artem Shatylo <alukard32244@gmail.com>
2025-05-08 00:58:06 +0300

    add and init alenbic. hide webhook data.

--- app/core/config.py
+++ app/core/config.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 class Settings:
     API_TOKEN = os.getenv("BOT_TOKEN")
-    WEBHOOK_URL = "https://8c98-91-203-143-82.ngrok-free.app/webhook"
-    WEBAPP_HOST = "localhost"
-    WEBAPP_PORT = 8000
+    WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+    WEBAPP_HOST = os.getenv("HOST", "localhost")
+    WEBAPP_PORT = int(os.getenv("PORT", 8000))
 
 
 settings = Settings()
